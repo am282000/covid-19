20b1b8201d0e418084a0d891aeb80be0d1f00cc0
Ashish Madan <madaansahaab777@gmail.com>
2020-12-25 05:07:31 +0530

    done

---
+++ script.js
@@ -0,0 +1,78 @@
+window.onload = function() {
+	getCovidStats();
+}
+
+function getCovidStats() {
+	fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/145')
+	.then(function(resp) { return resp.json() })
+	.then(function(data) {
+		let population = data.location.country_population;
+		let update = data.location.last_updated;
+		let confirmedCases = data.location.latest.confirmed;
+		let deaths = data.location.latest.deaths;
+
+		document.getElementById('population').innerHTML = population.toLocaleString('en');
+		document.getElementById('update').innerHTML = update.substr(0, 10);
+		document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
+		document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
+		document.getElementById('percent').innerHTML = ((Number(deaths)/Number(confirmedCases))*100).toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2}) + "%";
+
+	})
+	.catch(function() {
+		console.log("error");
+	})
+	setTimeout(getCovidStats, 43200000) // update every 12 hours
+}
+
+
+
+
+// stop redirection of contact form
+
+window.addEventListener("DOMContentLoaded", function () {
+
+	// get the form elements defined in your form HTML above
+  
+	var form = document.getElementById("my-form");
+	var status = document.getElementById("status");
+  
+	// Success and Error functions for after the form is submitted
+  
+	function success() {
+	  form.reset();
+	  status.classList.add('success');
+	  status.innerHTML = "Message Sent Succesfully!";
+	}
+  
+	function error() {
+	  status.classList.add('error');
+	  status.innerHTML = "Oops! There was a problem.";
+	}
+  
+	// handle the form submission event
+  
+	form.addEventListener("submit", function (ev) {
+	  ev.preventDefault();
+	  var data = new FormData(form);
+	  ajax(form.method, form.action, data, success, error);
+	});
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+	  if (xhr.readyState !== XMLHttpRequest.DONE) return;
+	  if (xhr.status === 200) {
+		success(xhr.response, xhr.responseType);
+	  } else {
+		error(xhr.status, xhr.response, xhr.responseType);
+	  }
+	};
+	xhr.send(data);
+  }
+  
+  
